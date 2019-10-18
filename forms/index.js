ca36efdae3246150303882a142bacbcbae6d0c5f
manavm1990 <manavm@visionwebsoft.com>
2019-10-18 15:27:29 -0500

    ✨Add form validation.

--- forms/index.js
+++ forms/index.js
@@ -1,3 +1,46 @@
-export default () => {
-  console.log('hello contact');
+import contact from "../store/Contact";
+
+const { validators } = contact;
+
+const customValidator = {
+  validateTextarea(
+    val,
+    {
+      textarea: { minLength, maxLength }
+    }
+  ) {
+    if (val.length > minLength && val.length > maxLength) {
+      return true;
+    }
+
+    return false;
+  }
+};
+
+function updateValidationClasses(el, isValid) {
+  if (isValid) {
+    el.classList.remove("is-invalid");
+    el.classList.add("is-valid");
+  } else {
+    el.classList.remove("invalid");
+    el.classList.add("is-invalid");
+  }
 }
+
+export default () => {
+  const inputs = document.querySelectorAll("input[id], textarea");
+
+  inputs.forEach(input => {
+    input.addEventListener("blur", function() {
+      const val = this.value;
+      const patt = this.pattern;
+
+      patt
+        ? updateValidationClasses(this, RegExp(patt).test(val))
+        : updateValidationClasses(
+            this,
+            customValidator.validateTextarea(val, validators)
+          );
+    });
+  });
+};
