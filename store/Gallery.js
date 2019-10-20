60a0614530710a2de513072c85484285cd1fefb7
manavm1990 <manavm@visionwebsoft.com>
2019-10-20 10:26:42 -0500

    :lipstick: Add photo modal.

--- store/Gallery.js
+++ store/Gallery.js
@@ -1,6 +1,11 @@
 export default {
   heading: "Gallery!",
-  main: `<button id="add-pic">Add Photo</button>
+  main: `
+  <div id="modal" class="is-hiding">
+    <video></video>
+    <button class="fas fa-camera-retro"></button>
+  </div>
+  <button id="add-pic">Add Photo</button>
     <div class="fotos">
       <p>No photos yet!</p>
     </div>
