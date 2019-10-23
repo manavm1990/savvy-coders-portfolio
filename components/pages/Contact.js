2e34de165cdeb08659f6fb665a9f2703dc828803
manavm1990 <manavm@visionwebsoft.com>
2019-10-23 11:35:20 -0500

    :lipstick: Clean up `<form>` layout on 'tablet.'

--- components/pages/Contact.js
+++ components/pages/Contact.js
@@ -1,28 +1,27 @@
 export default () => `
-    <form action="#" method="POST" data-netlify="true">
-        <div class="flex-container--desktop flex-row--desktop">
-        <div>
-            <label for="name">Name:</label>
-            <input type="text" name="name" id="name">
-          </div>
+  <form action="#" method="POST" data-netlify="true">
+    <div class="flex-container--tablet flex-row--tablet">
+      <div>
+        <label for="name">Name:</label>
+        <input type="text" name="name" id="name">
+      </div>
 
-          <div>
-            <label for="email">Email:</label>
-            <input type="email" name="email" id="email">
-          </div>
+      <div>
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email">
+      </div>
 
-          <div>
-            <label for="fone">Phone:</label>
-            <input type="tel" name="fone" id="fone">
-          </div>
-        </div>
+      <div>
+        <label for="fone">Phone:</label>
+        <input type="tel" name="fone" id="fone">
+      </div>
+    </div>
 
-        <div>
-          <label for="msg">Enter your message:</label>
-          <textarea name="msg" id="msg" cols="30" rows="10"></textarea>
-        </div>
+    <div>
+      <label for="msg">Enter your message:</label>
+      <textarea name="msg" id="msg" cols="30" rows="10"></textarea>
+    </div>
 
-        <input type="submit" value="Submit">
-
-      </form>
+    <input type="submit" value="Submit">
+  </form>
 `;
