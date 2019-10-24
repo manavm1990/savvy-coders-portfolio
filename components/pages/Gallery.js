2e192a17839f76d98cc472c9b0a84f50b307a6b3
manavm1990 <manavm@visionwebsoft.com>
2019-10-24 09:05:45 -0500

    :rewind: Bring back 'add photo' button and canvas and modal.

--- components/pages/Gallery.js
+++ components/pages/Gallery.js
@@ -9,11 +9,24 @@ function getFiguresHTML(pics) {
         <!-- TODO: Add <figcaption>. (use modal 👆🏾) -->
       </figure>
 `
-  );
+  ).join(" ");
 }
 
-export default s => `
+export default s => {
+ console.log('gallery got state', s)
+  return `
+  <!-- Just a holding spot to capture the image - not to be seen. -->
+  <canvas></canvas>
+
+  <div id="modal" class="is-hiding">
+    <video></video>
+    <button class="fas fa-camera-retro" id="take-pic"></button>
+  </div>
+
+  <button id="add-pic">Add Photo</button>
+
   <div class="fotos">
     ${getFiguresHTML(s.pics)}
   </div>
   `
+};
