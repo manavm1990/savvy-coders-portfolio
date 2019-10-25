92594bd677645213011d1111f1c1a1feaf427d3b
manavm1990 <manavm@visionwebsoft.com>
2019-10-25 09:44:28 -0500

    :see_no_evil: Add firebase config to repo for Netlify.

---
+++ firebase/config.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyDX6XLe0E4ibfIMyEsZWykwqgsuBP1xGdg",
+  authDomain: "savvy-coders-practice.firebaseapp.com",
+  databaseURL: "https://savvy-coders-practice.firebaseio.com",
+  projectId: "savvy-coders-practice",
+  storageBucket: "savvy-coders-practice.appspot.com",
+  messagingSenderId: "14926798271",
+  appId: "1:14926798271:web:497ed2870baead8582550a"
+};
