836461d08330082f2666ae438234c266bcc239eb
manavm1990 <manavm@visionwebsoft.com>
2019-10-27 08:58:46 -0500

    ♻️Refactor functions for less 'bulkiness'.
    We demo named exports also.  https://exploringjs.com/es6/ch_modules.html#_multiple-named-exports

--- pics/index.js
+++ pics/index.js
@@ -1,154 +1,12 @@
-import { auth, db } from "../firebase";
-
-const dbCollection = db.collection("pics");
-
-function admin() {
-  const modal = document.querySelector("#modal--admin");
-
-  modal.querySelector("#sign-out").addEventListener("click", () => {
-    auth
-      .signOut()
-      .then(() => {
-        toggleModal(modal);
-        authorize();
-      })
-      .catch(err => console.error("Sign Out error!", err.message));
-  });
-
-  toggleModal(modal);
-}
-
-function authorize() {
-  const modal = document.querySelector("#modal--auth");
-  const error = modal.querySelector(".error");
-
-  error.textContent = "";
-
-  modal.querySelectorAll("button").forEach(btn =>
-    btn.addEventListener("click", function() {
-      if (this.textContent === "Login") {
-        auth
-          .signInWithEmailAndPassword(
-            modal.querySelector("#email").value,
-            modal.querySelector("#pass").value
-          )
-          .then(() => {
-            toggleModal(modal);
-            admin();
-          })
-          .catch(err => {
-            modal.querySelector(".error").textContent = err.message;
-          });
-      } else {
-        // Must be the sign up button.
-        auth.createUserWithEmailAndPassword(
-          modal.querySelector("#email").value,
-          modal.querySelector("#pass").value
-        ).then(() => {
-            toggleModal(modal);
-            admin();
-          })
-          .catch(err => {
-            modal.querySelector(".error").textContent = err.message;
-          });
-      }
-    })
-  );
-
-  modal.querySelectorAll("input").forEach(i => {
-    // Clear error message if user tries again by focusing on input
-    i.addEventListener("focus", () => {
-      error.textContent = "";
-    });
-  });
-
-  toggleModal(modal);
-}
-
-function camera(st) {
-  const modal = document.querySelector("#modal--camera");
-
-  const video = document.querySelector("video");
-  const canvas = document.querySelector("canvas");
-
-  toggleModal(modal);
-
-  /**
-   * `navigator` represents the browser.
-   *
-   * `mediaDevices` is another WPI allowing interfaces with...media devices 😏
-   */
-  navigator.mediaDevices
-    // `getUserMedia prompts user for remission
-    .getUserMedia({ video: true, audio: false })
-    .then(function(localMediaStream) {
-      // Get the stream into `<video>`
-      video.srcObject = localMediaStream;
-
-      // Start playing to see something! 😃
-      video.play();
-    })
-    .catch(function(err) {
-      console.log("An error occurred: " + err);
-    });
-
-  document.querySelector("#take-pic").addEventListener("click", () => {
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-
-    // This is what actually takes the picture from the webcam
-    canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height);
-
-    toggleModal(modal);
-
-    const newPic = {
-      src: canvas.toDataURL("image/webp"),
-      caption: document.querySelector("#caption").value
-    };
-
-    // Developer's Note: `push` will not work as it just `return`s `length` of Array
-    // Wrap newPic in Array so we can use `concat()` and trigger PROXY's SET TRAP.
-    st.pics = st.pics.concat([ritePic(newPic)]);
-  });
-}
-
-function fileReader(file) {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-
-    // The load event is fired when a file has been read successfully (MDN)
-    reader.addEventListener("load", () => {
-      // Send back the URL info from 👆🏾
-      resolve(reader.result);
-    });
-
-    reader.addEventListener("error", err => {
-      reject(err);
-    });
-  });
-}
-
-function ritePic(pic) {
-  /**
-   * Developer's Note: Since we are just using base64 URL encoded string,
-   * if the pic is too big, it will error out!
-   */
-  dbCollection
-    .add(pic)
-    .then(docRef => {
-      // Get the id in case we need to delete it
-      pic.id = docRef.id;
-    })
-    .catch(() => (pic.caption = "This pic is too big! Try another!"));
-
-  return pic;
-}
-
-function toggleModal(modal) {
-  modal.classList.toggle("is-hiding");
-  modal.classList.toggle("is-showing");
-}
+import {
+  dbConnection,
+  handleAdminModal,
+  handleCameraModal,
+  handleLoginModal,
+  readFile,
+  ritePic,
+  toggleModal
+} from "./lib";
 
 export default st => {
   const closeBtns = document.querySelectorAll(".fa-window-close");
@@ -156,25 +14,23 @@ export default st => {
 
   // If no pics, let's get some - otherwise, no need to.
   if (!st.pics.length) {
-    db.collection("pics")
-      .get()
-      .then(
-        querySnapshot =>
-          (st.pics = querySnapshot.docs.map(doc => {
-            const pic = doc.data();
-            pic.id = doc.id;
-            return pic;
-          }))
-      );
+    dbConnection.get().then(
+      querySnapshot =>
+        (st.pics = querySnapshot.docs.map(doc => {
+          const pic = doc.data();
+          pic.id = doc.id;
+          return pic;
+        }))
+    );
   }
 
   document.querySelector("#add-pic").addEventListener("click", () => {
     // Pass in st instead of just st.pics so that it can trigger the PROXY SET TRAP.
-    camera(st);
+    handleCameraModal(st);
   });
 
   document.querySelector("#upload-pic").addEventListener("change", function() {
-    fileReader(this.files[0])
+    readFile(this.files[0])
       .then(response => {
         const newPic = {
           src: response
@@ -189,9 +45,9 @@ export default st => {
 
   document.querySelector("#url-pic").addEventListener("click", () => {
     if (st.isAuth) {
-      admin();
+      handleAdminModal();
     } else {
-      authorize();
+      handleLoginModal();
     }
   });
 
@@ -207,10 +63,14 @@ export default st => {
       const figure = this.closest("figure");
 
       // https://firebase.google.com/docs/firestore/manage-data/delete-data
-      dbCollection
+      dbConnection
         .doc(figure.dataset.id)
         .delete()
         .then(() => {
+          /**
+           * TODO: Update `state` to reflect the deletion.
+           * Currently, we will not see the update until we hit firestore manually with a refresh.
+           */
           // https://developer.mozilla.org/en-US/docs/Web/API/ChildNode/remove
           figure.remove();
         });
---
+++ pics/lib.js
@@ -0,0 +1,153 @@
+import { auth, db } from "../firebase"
+
+export const dbConnection = db.collection("pics");
+
+export function handleAdminModal() {
+  const modal = document.querySelector("#modal--admin");
+
+  modal.querySelector("#sign-out").addEventListener("click", () => {
+    auth
+      .signOut()
+      .then(() => {
+        toggleModal(modal);
+        handleLoginModal();
+      })
+      .catch(err => console.error("Sign Out error!", err.message));
+  });
+
+  toggleModal(modal);
+}
+
+export function handleLoginModal() {
+  const modal = document.querySelector("#modal--auth");
+  const error = modal.querySelector(".error");
+
+  error.textContent = "";
+
+  modal.querySelectorAll("button").forEach(btn =>
+    btn.addEventListener("click", function() {
+      if (this.textContent === "Login") {
+        auth
+          .signInWithEmailAndPassword(
+            modal.querySelector("#email").value,
+            modal.querySelector("#pass").value
+          )
+          .then(() => {
+            toggleModal(modal);
+            handleAdminModal();
+          })
+          .catch(err => {
+            modal.querySelector(".error").textContent = err.message;
+          });
+      } else {
+        // Must be the sign up button.
+        auth
+          .createUserWithEmailAndPassword(
+            modal.querySelector("#email").value,
+            modal.querySelector("#pass").value
+          )
+          .then(() => {
+            toggleModal(modal);
+            handleAdminModal();
+          })
+          .catch(err => {
+            modal.querySelector(".error").textContent = err.message;
+          });
+      }
+    })
+  );
+
+  modal.querySelectorAll("input").forEach(i => {
+    // Clear error message if user tries again by focusing on input
+    i.addEventListener("focus", () => {
+      error.textContent = "";
+    });
+  });
+
+  toggleModal(modal);
+}
+
+export function handleCameraModal(st) {
+  const modal = document.querySelector("#modal--camera");
+
+  const video = document.querySelector("video");
+  const canvas = document.querySelector("canvas");
+
+  toggleModal(modal);
+
+  /**
+   * `navigator` represents the browser.
+   *
+   * `mediaDevices` is another WPI allowing interfaces with...media devices 😏
+   */
+  navigator.mediaDevices
+    // `getUserMedia prompts user for remission
+    .getUserMedia({ video: true, audio: false })
+    .then(function(localMediaStream) {
+      // Get the stream into `<video>`
+      video.srcObject = localMediaStream;
+
+      // Start playing to see something! 😃
+      video.play();
+    })
+    .catch(function(err) {
+      console.log("An error occurred: " + err);
+    });
+
+  document.querySelector("#take-pic").addEventListener("click", () => {
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    // This is what actually takes the picture from the webcam
+    canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    toggleModal(modal);
+
+    const newPic = {
+      src: canvas.toDataURL("image/webp"),
+      caption: document.querySelector("#caption").value
+    };
+
+    // Developer's Note: `push` will not work as it just `return`s `length` of Array
+    // Wrap newPic in Array so we can use `concat()` and trigger PROXY's SET TRAP.
+    st.pics = st.pics.concat([ritePic(newPic)]);
+  });
+}
+
+export function readFile(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    // The load event is fired when a file has been read successfully (MDN)
+    reader.addEventListener("load", () => {
+      // Send back the URL info from 👆🏾
+      resolve(reader.result);
+    });
+
+    reader.addEventListener("error", err => {
+      reject(err);
+    });
+  });
+}
+
+export function ritePic(pic) {
+  /**
+   * Developer's Note: Since we are just using base64 URL encoded string,
+   * if the pic is too big, it will error out!
+   */
+  dbConnection
+    .add(pic)
+    .then(docRef => {
+      // Get the id in case we need to delete it
+      pic.id = docRef.id;
+    })
+    .catch(() => (pic.caption = "This pic is too big! Try another!"));
+
+  return pic;
+}
+
+export function toggleModal(modal) {
+  modal.classList.toggle("is-hiding");
+  modal.classList.toggle("is-showing");
+}
